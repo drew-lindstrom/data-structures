77dd72380fe891da22c6df17b8f8cc919ce75cbd
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-02-15 10:59:18 -0600

    Added insertion sort.

--- sorting.py
+++ sorting.py
@@ -2,12 +2,11 @@ array = [10, 8, 3, 5, 2, 9, 7, 1, 0, 4]
 
 
 def selection_sort(array):
-    max_ = None
-    max_pos = None
-
     i = len(array)
 
     while i > 0:
+        max_ = None
+        max_pos = None
         for n in range(i):
             if (max_ == None) or (array[n] > max_):
                 max_ = array[n]
@@ -15,10 +14,18 @@ def selection_sort(array):
             if n == (i - 1):
                 array[max_pos], array[n] = array[n], array[max_pos]
         i -= 1
-        max_ = None
-        max_pos = None
 
     return array
 
 
-print(selection_sort(array))
+def insertion_sort(array):
+    for i in range(len(array)):
+        j = i
+        while j > 0 and array[j - 1] > array[j]:
+            array[j], array[j - 1] = array[j - 1], array[j]
+            j = j - 1
+
+    return array
+
+
+print(insertion_sort(array))
