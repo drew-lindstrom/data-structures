031218a559a65aab693c4e0b4e3286a022d967a0
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-02-15 19:10:53 -0600

    Added bubble sort.

--- sorting.py
+++ sorting.py
@@ -28,4 +28,13 @@ def insertion_sort(array):
     return array
 
 
-print(insertion_sort(array))
+def bubble_sort(array):
+    for i in range(len(array)):
+        for j in range(len(array) - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+
+    return array
+
+
+print(bubble_sort(array))
