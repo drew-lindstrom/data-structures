2106872930e8a173d459af437c05cba05ea7bea3
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-02-17 08:27:25 -0600

    Implemented insert_at_head and print_list

--- linked_list.py
+++ linked_list.py
@@ -11,8 +11,25 @@ class LinkedList:
     def get_head(self):
         return self.head
 
+    def insert_at_head(self, data):
+        temp_node = Node(data)
+        temp_node.next_node = self.head
+        self.head = temp_node
+        return self.head
+
     def is_empty(self):
         if self.head is None:
             return True
         else:
             return False
+
+    def print_list(self):
+        if self.isEmpty():
+            print("List is Empty")
+            return False
+        temp = self.head_node
+        while temp.next_element is not None:
+            print(temp.data, end=" -> ")
+            temp = temp.next_node
+        print(temp.data, "-> None")
+        return True
