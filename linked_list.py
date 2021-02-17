fd5400143ba433fc02232dbeadb6e22b6c776955
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-02-17 08:21:49 -0600

    Added get_head and is_empty for linked_list

--- linked_list.py
+++ linked_list.py
@@ -7,3 +7,12 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+
+    def get_head(self):
+        return self.head
+
+    def is_empty(self):
+        if self.head is None:
+            return True
+        else:
+            return False
