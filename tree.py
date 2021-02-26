913d807cc3785958aff58db93d200b1e6f09ee4c
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-02-26 15:16:11 -0600

    Implemented iterative and recursive insert.

--- tree.py
+++ tree.py
@@ -3,11 +3,49 @@ class Node:
         self.left = None
         self.right = None
         self.data = data
-        self.parent = None
 
+    #Iterative insert
+    def insert(self, data):
+        current = self
+        parent = None
+
+        while current:
+            parent = current
+            if data < current.data:
+                current = current.left
+            else:
+                current = current.right
+        
+        if data < parent.data:
+            parent.left = Node(data)
+        else:
+            parent.right = Node(data)
+
+    #Recursive insert
+    def insert(self, data):
+        if data < self.data:
+            if self.left:
+                self.left.insert(data)
+            else:
+                self.left = Node(data)
+                return
+        else:
+            if self.right:
+                self.right.insert(data)
+            else:
+                self.right = Node(data)
+                return 
+                
 class BinarySearchTree:
     def __init__(self):
         self.root = Node(data)
+
+    def insert(self, data):
+        if self.root:
+            return self.root.insert(data)
+        else:
+            self.root = Node(data)
+            return True
 
 
     def insert(self, data):
