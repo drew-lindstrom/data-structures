6690f18fa0b304c8fd0f42e19030d095883a3419
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-03-10 07:45:04 -0600

    Implemented basic recursive solution to find_min_fee problem.

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -10,3 +10,23 @@ def count_min_jumps(jumps):
             end += 1
 
     return dp[n - 1]
+
+
+def find_min_fee(fee):
+    return find_min_fee_recursive(fee, 0)
+
+
+def find_min_fee_recursive(fee, step):
+    if step > len(fee) - 1:
+        return 0
+
+    take_1_step = find_min_fee_recursive(fee, step + 1)
+    take_2_step = find_min_fee_recursive(fee, step + 2)
+    take_3_step = find_min_fee_recursive(fee, step + 3)
+
+    _min = min(take_1_step, take_2_step, take_3_step)
+
+    return _min + fee[step]
+
+
+print(find_min_fee([1, 2, 5, 2]))
