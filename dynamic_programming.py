a40b819aebe1152e9c66901af8132186185ab3e9
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-03-11 09:16:45 -0600

    Implemented tabulation solution to find_lps_length problem.

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -103,4 +103,23 @@ def find_lps_length_dynamic(string, pointer1, pointer2, dp):
     return dp[pointer1][pointer2]
 
 
-print(find_lps_length("abdbca"))
+def find_lps_length_tabulation(string):
+    n = len(string)
+    dp = [[-1 for _ in range(n)] for _ in range(n)]
+
+    for i in range(n):
+        dp[i][i] = 1
+
+    for startIndex in range(n - 1, -1, -1):
+        for endIndex in range(startIndex + 1, n):
+            if string[startIndex] == string[endIndex]:
+                dp[startIndex][endIndex] = 2 + dp[startIndex + 1][endIndex - 1]
+            else:
+                dp[startIndex][endIndex] = max(
+                    dp[startIndex + 1][endIndex], dp[startIndex][endIndex - 1]
+                )
+
+    return dp[0][n - 1]
+
+
+print(find_lps_length_tabulation("abdbca"))
