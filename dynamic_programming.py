32921ef5297382f883f94d0575ed504f49f06ea9
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-03-12 08:39:29 -0600

    Implemented recursive solution to longest palindrome problem.

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -122,4 +122,29 @@ def find_lps_length_tabulation(string):
     return dp[0][n - 1]
 
 
-print(find_lps_length_tabulation("abdbca"))
+def find_LPS_length(st):
+    return find_LPS_length_recursive(st, 0, len(st) - 1)
+
+
+def find_LPS_length_recursive(st, startIndex, endIndex):
+    if startIndex > endIndex:
+        return 0
+
+    if startIndex == endIndex:
+        return 1
+
+    if st[startIndex] == st[endIndex]:
+        remainingLength = endIndex - startIndex - 1
+        if remainingLength == find_LPS_length_recursive(
+            st, startIndex + 1, endIndex - 1
+        ):
+            return remainingLength + 2
+        # else:
+        #     return
+
+    c1 = find_LPS_length_recursive(st, startIndex + 1, endIndex)
+    c2 = find_LPS_length_recursive(st, startIndex, endIndex - 1)
+    return max(c1, c2)
+
+
+print(find_LPS_length("abdbca"))
