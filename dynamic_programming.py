1e4b82026603ab21fc82497fca5cad3456aa1ce7
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-03-12 08:53:45 -0600

    Impletmented memoization solution to longest palindrome problem.

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -122,10 +122,6 @@ def find_lps_length_tabulation(string):
     return dp[0][n - 1]
 
 
-def find_LPS_length(st):
-    return find_LPS_length_recursive(st, 0, len(st) - 1)
-
-
 def find_LPS_length_recursive(st, startIndex, endIndex):
     if startIndex > endIndex:
         return 0
@@ -139,12 +135,39 @@ def find_LPS_length_recursive(st, startIndex, endIndex):
             st, startIndex + 1, endIndex - 1
         ):
             return remainingLength + 2
-        # else:
-        #     return
 
     c1 = find_LPS_length_recursive(st, startIndex + 1, endIndex)
     c2 = find_LPS_length_recursive(st, startIndex, endIndex - 1)
     return max(c1, c2)
+
+
+def find_LPS_length(st):
+    n = len(st)
+    dp = [[-1 for _ in range(n)] for _ in range(n)]
+    return find_LPS_length_dp(dp, st, 0, n - 1)
+
+
+def find_LPS_length_dp(dp, st, startIndex, endIndex):
+    if startIndex > endIndex:
+        return 0
+
+    if startIndex == endIndex:
+        return 1
+
+    if dp[startIndex][endIndex] == -1:
+        if st[startIndex] == st[endIndex]:
+            remainingLength = endIndex - startIndex - 1
+            if remainingLength == find_LPS_length_dp(
+                dp, st, startIndex + 1, endIndex - 1
+            ):
+                dp[startIndex][endIndex] = remainingLength + 2
+                return dp[startIndex][endIndex]
+
+        c1 = find_LPS_length_dp(dp, st, startIndex + 1, endIndex)
+        c2 = find_LPS_length_dp(dp, st, startIndex, endIndex - 1)
+        dp[startIndex][endIndex] = max(c1, c2)
+
+    return dp[startIndex][endIndex]
 
 
 print(find_LPS_length("abdbca"))
