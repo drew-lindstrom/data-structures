1270c09ef6ba4b81eea5a9677e7370663997a22c
Drew Lindstrom <drew.k.lindstrom@gmail.com>
2021-03-13 17:35:52 -0600

    Added option to clear screen.

--- a_star_path_finding.py
+++ a_star_path_finding.py
@@ -205,16 +205,12 @@ def main(win, width):
     end = None
 
     run = True
-    started = False
 
     while run:
         draw(win, grid, ROWS, width)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
-            if started:
-                continue
 
             if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
@@ -242,13 +238,17 @@ def main(win, width):
                     end = None
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and not started:
+                if event.key == pygame.K_SPACE and start and end:
                     for row in grid:
                         for spot in row:
                             spot.update_neighbors(grid)
 
                     algorithm(lambda: draw(win, grid, ROWS, width), grid, start, end)
 
+                if event.key == pygame.K_c:
+                    start = None
+                    end = None
+                    grid = make_grid(ROWS, width)
     pygame.quit()
 
 
